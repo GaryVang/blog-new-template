3b9869973bbcc0dd67655947e952fd971a5064d7
MysticV <leng_vang89@hotmail.com>
2020-05-08 20:00:29 -0700

    Deployment changes.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import Home from './components/Pages/Home';
 import Blog from './components/Pages/Blog';
 import "./App.css";
 
-
 function App() {
   return (
-    <Router>
+    <Router >
       <div className="w-blog">
         <Header className="header"></Header>
         {/* <content className='content'>Welcome to G's Blog!</content> */}
@@ -22,10 +21,7 @@ function App() {
         <Route path='/signup' component={Signup} />
       </div>
     </Router>
-    
   );
 }
-
-
 
 export default App;
