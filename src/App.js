3bac12083679984c3c57e34ecd6a60fb84a2d1ec
MysticV <leng_vang89@hotmail.com>
2020-05-08 20:18:34 -0700

    Corrected the import statment.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link, browserHistory } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {browserHistory} from 'react-router';
 import Header from './components/Header/Header.js';
 import Login from './components/Pages/Login';
 import Signup from './components/Pages/Signup';
